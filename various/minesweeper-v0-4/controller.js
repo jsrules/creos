f964ba6be38c12d309a9e16ea92c9309a33b436f
jsrules <43886679+jsrules@users.noreply.github.com>
2023-04-06 01:36:02 +0300

    Add files via upload

---
+++ various/minesweeper-v0-4/controller.js
@@ -0,0 +1,112 @@
+////////////////////////////////////////////////////////// controller ///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+'use strict';
+
+// alert(554)
+
+function stopEvent(event) {
+    if (event.preventDefault != undefined)
+        event.preventDefault();
+    if (event.stopPropagation != undefined)
+        event.stopPropagation();
+}
+
+
+function imgRightClickHandler(img) {
+    // alert(100000000)
+    if (getFilenameFromURL(img.src) != getFilenameFromURL(START_SQWARE_IMG) && getFilenameFromURL(img.src) != getFilenameFromURL(MARKED_SQWARE_IMG)) {
+        return
+    }
+
+
+    // img.src = getFilenameFromURL(img.src) != getFilenameFromURL(MARKED_SQWARE_IMG) ? MARKED_SQWARE_IMG : START_SQWARE_IMG;
+
+
+    if (getFilenameFromURL(img.src) != getFilenameFromURL(MARKED_SQWARE_IMG)) {
+        img.src = MARKED_SQWARE_IMG
+        minesLeft--
+    } else {
+        img.src = START_SQWARE_IMG
+        minesLeft++
+    }
+    redrawMinesLeft()
+
+    if (minesLeft == 0) {
+        isGameOver = true;
+        redrawBoard(false)
+        // alert('Done!')
+    }
+
+
+    return false
+}
+
+
+function imgClickHandler(img) {
+    // alert(554);
+    // img.src = MARKED_SQWARE_IMG;// 'img/marked-square.jpg'
+
+    let id = img.id;
+    let x = breakId(id)[0]
+    let y = breakId(id)[1]
+
+    if (getFilenameFromURL(img.src) != getFilenameFromURL(START_SQWARE_IMG)) {
+        return
+    }
+    if (isThereAMineAt(x, y)) {
+        bangMineId = id;
+        isGameOver = true
+        gameOverMsg = FAIL_GAMEOVER_MESSAGE
+        redrawBoard(false)
+        // myAlert('BANG !!!')
+    } else {  //showing neighbourhood
+        // img.src = EMPTY_SQWARE_IMG;
+        let numberOfNeighbourhoodMines = findNumberOfNeighbourhoodMines(x, y)
+        img.src = EMPTY_SQWARE_WITH_NUMBER_IMG_FIRST_PART + String(numberOfNeighbourhoodMines) + EMPTY_SQWARE_WITH_NUMBER_IMG_LAST_PART;
+
+
+        if (numberOfNeighbourhoodMines !== 0) {
+            return;
+        }
+
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                try {
+                    let currNeighborhoodImgId = createId(x + i, y + j)
+                    let currNeighborhoodImg = $(currNeighborhoodImgId)
+                    // currNeighborhoodImg.src = START_SQWARE_IMG;
+                    // log(getFilenameFromURL(currNeighborhoodImg.src))
+
+                    if (getFilenameFromURL(currNeighborhoodImg.src) == getFilenameFromURL(START_SQWARE_IMG) && (!isThereAMineAt(x + i, y + j))) {
+                        // currNeighborhoodImg.click();
+                        setTimeout(() => {
+                            currNeighborhoodImg.click();
+                        }, 1);
+                    }
+
+
+                    // var currValue = modelArr[modelArrWidth + i][modelArrHeight + j]
+                } catch (error) {
+                    // log('EXCEPTION!! : ' + error)
+                }
+
+                // total += (!isNaN(currValue) ? currValue : 0);
+            }
+        }
+    }
+
+    // let nGumberOfNeighbourhoodMines = findNumberOfNeighbourhoodMines(x, y)
+    // alert(numberOfNeighbourhoodMines)
+
+}
+
+function documentDoubleClick(e) {
+    // alert(554);
+    // img.src = MARKED_SQWARE_IMG;// 'img/marked-square.jpg'
+    alert(e)
+}
+
---
+++ various/minesweeper-v0-4/view.js
@@ -0,0 +1,139 @@
+////////////////////////////////////////////////////////// view ///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+'use strict';
+
+/////////////////////////////////////////////////////
+function myAlert(s) {
+    setTimeout("alert('" + s + "')", 200);
+}
+
+/////////////////////////////////////////////////////
+function myAlertAndReloadThePage(s) {
+    setTimeout("alert('" + s + "');  location.reload();", 200);
+}
+
+/////////////////////////////////////////////////////
+//gets 1 and 3 and returns string "1,3"
+function createId(first, second) {
+    return first + COMMA + second;
+}
+
+/////////////////////////////////////////////////////
+//gets string like "1,3" and returns array [1,3]
+function breakId(id) {
+    var pos = id.indexOf(COMMA);
+    return [parseInt(id.slice(0, pos)), parseInt(id.slice(pos + 1, id.length))];
+}
+
+function redrawMinesLeft() {
+    $('spanWithNumberOfminesLeftId').innerHTML = minesLeft;
+}
+
+function redrawBoard(isStart) {
+    let htmlToAdd = '';
+    let imgToDraw = null;
+
+    for (let index1 = 0; index1 < FIELD_HEIGHT; index1++) {
+
+        for (let index = 0; index < FIELD_WIDTH; index++) {
+
+            if (isStart) {
+                imgToDraw = START_SQWARE_IMG
+            }
+            else {
+                let numberOfNeighbourhoodMines = findNumberOfNeighbourhoodMines(index, index1)
+                let imgToUse = EMPTY_SQWARE_WITH_NUMBER_IMG_FIRST_PART + String(numberOfNeighbourhoodMines) + EMPTY_SQWARE_WITH_NUMBER_IMG_LAST_PART;
+                imgToDraw = modelArr[index][index1] == 0 ? imgToUse : (createId(index, index1) == bangMineId ? MINE_SQWARE_IMG : MINE_SQWARE_IMG_GREEN);
+                if (isGameOver) {
+                    gameOverMsg != FAIL_GAMEOVER_MESSAGE && (gameOverMsg = 'Success!!! Congrats, you won.')
+                    let imgOnTheExistingBoard = $(createId(index, index1))
+                    if (getFilenameFromURL(imgOnTheExistingBoard.src) == getFilenameFromURL(MARKED_SQWARE_IMG) && !isThereAMineAt(index, index1)) {
+                        imgToDraw = FALSELY_MARKED_SQWARE_IMG;
+                        gameOverMsg = FAIL_GAMEOVER_MESSAGE
+                    }
+                }
+            }
+
+            let idToUse = createId(index, index1)
+            htmlToAdd += '<img id="' + idToUse + '" style="display:inline; Font-Size:0px; Line-Height: 0px; width:30px;height:30px;margin: 0px;  padding: 0px;border: none;" border="0" src="' + imgToDraw +
+                '" alt="" onClick="imgClickHandler(this)" oncontextmenu="imgRightClickHandler(this)"  role="button"  tabIndex="0" />';
+
+        }
+
+        htmlToAdd += '<br>'
+    }
+    $('fieldDivId').innerHTML = htmlToAdd
+    gameOverMsg && myAlert(gameOverMsg)
+}
+
+
+
+
+function draw() {
+    // return
+
+    // alert(5)
+    // $('fieldDivId').innerHTML ='fieldDivId !!!1111' + new Date()
+
+
+    // alert( $('fieldDivId').innerHTML )
+
+    redrawBoard(true)
+    return;
+
+    // let r = 0//randomInteger(0, 255)
+    // let g = 0// randomInteger(0, 255)
+    // let b = 0//randomInteger(0, 255)
+    // let alfa = 255
+
+
+    currX = currX < END_X ? currX + 1 : 0;
+    // currY = currY < END_Y ? currY + 1 : 0;
+    currR = currR < 255 ? currR + 1 : 0;
+
+
+    let r = 255 // randomInteger(0, 255)
+    let g = 255 //randomInteger(0, 255)  //randomInteger(0, 255)
+    let b = randomInteger(0, 255)
+    let alfa = 255
+
+    verticalLine(currX, 0, END_Y, r, g, b, alfa);
+    // horizontalLine(0, END_X, currY, r, g, b, alfa);
+
+
+    // //copypasted from verticalLine()
+    // for (let index = 0; index <= END_X; index++) {
+    //     const currX = index;
+    //     // putPixel(currX,  (((k * currX) + b)))  // add  var "b" (kx+b)
+    //     let r = randomInteger(0, 255)
+    //     let g = randomInteger(0, 255)
+    //     let b = randomInteger(0, 255)
+    //     dot(currX, currY, r, g, b, alfa)
+    // }
+
+
+
+    // // hor:
+    // line(0, 100, END_X, 50, r, g, b, alfa)
+    // line(0, 100, END_X, 100, r, g, b, alfa)
+    // line(0, 100, END_X, 250, r, g, b, alfa)
+    // line(0, 100, END_X, 270, r, g, b, alfa)
+    // line(0, 100, END_X, 300, r, g, b, alfa)
+    // line(0, 100, END_X, 150, r, g, b, alfa)
+    // line(0, 100, END_X, 25, r, g, b, alfa)
+    // line(0, 100, END_X, 50, r, g, b, alfa)
+    // line(0, 100, END_X, 0, r, g, b, alfa)
+
+    // // vert:
+    // line(100, 0, 100, END_Y, r, g, b, alfa)
+    // line(100, 0, 200, END_Y, r, g, b, alfa)
+
+
+
+    //RAMKA:
+    // horizontalLine(10, END_X - 10, 10, r, g, b, alfa)
+    // horizontalLine(10, END_X - 10, END_Y-10, r, g, b, alfa)
+    // verticalLine(END_X-10, 10, END_Y-10,  r, g, b, alfa)
+    // verticalLine(10, 10, END_Y-10,  r, g, b, alfa)
+
+
+}
