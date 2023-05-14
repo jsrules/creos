f110b2a1d4fea67adeee3a3b1798e40a1fea7524
jsrules <43886679+jsrules@users.noreply.github.com>
2023-05-15 00:40:00 +0300

    Add files via upload

---
+++ various/chess-v0-19/commons.js
@@ -0,0 +1,130 @@
+////////////////////////////////////////////////////////// commons ///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+'use strict';
+
+
+const hexes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+const MIN_COLOR = 0;
+const MAX_COLOR = 255;
+
+
+/////////////////////////////////////////////////////   
+function getColorOfPieceOnTd(x) {
+    if (x.innerHTML == '') {
+        return null
+    }
+    let html = x.innerHTML
+
+    var rx = /.*(\w)\w\.png/g;
+    var arr = rx.exec(html);
+    // log(arr[1])
+    return arr[1];
+
+}
+
+/////////////////////////////////////////////////////   
+function getColorOfPieceInHtml(html) {
+    // if (x.innerHTML == '') {
+    //     return null
+    // }
+    // let html = x.innerHTML
+
+    var rx = /.*\w(\w)\.png/g;
+    var arr = rx.exec(html);
+    // log(arr[1])
+    return arr[1];
+
+}
+
+/////////////////////////////////////////////////////   
+function opposingColor(x) {
+    return x == 'w' ? 'b' : 'w'
+
+}
+
+/////////////////////////////////////////////////////    solution from https://stackoverflow.com/questions/979975/get-the-values-from-the-get-parameters-javascript
+function getUrlParam(param) {
+    var url_string = self.location.href; //"http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
+    var url = new URL(url_string);
+    var c = url.searchParams.get(param);
+    // console.log('Param-param:'+c);
+    return c;
+}
+
+/////////////////////////////////////////////////////   
+function w(x) {
+    return document.write(x);
+
+}
+
+/////////////////////////////////////////////////////   
+function $(x) {
+    return document.getElementById(x);
+
+}
+
+/////////////////////////////////////////////////////   
+function log(s) {
+    console.log(s)
+}
+
+/////////////////////////////////////////////////////
+function ctg(x) { return 1 / Math.tan(x); }
+
+/////////////////////////////////////////////////////
+function arcctg(x) { return Math.PI / 2 - Math.atan(x); }
+
+/////////////////////////////////////////////////////   
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+/////////////////////////////////////////////////////   
+function randHexDigit() {
+    return hexes[randomInteger(0, 15)];
+}
+
+/////////////////////////////////////////////////////   
+function randColor() {
+    return "#" + randHexDigit() + randHexDigit() + randHexDigit() + randHexDigit() + randHexDigit() + randHexDigit();
+}
+
+/////////////////////////////////////////////////////   
+function randShadeOfRed() {
+    return "#" + randHexDigit() + randHexDigit() + '0000';
+}
+
+/////////////////////////////////////////////////////   
+function randShadeOfGreen() {
+    return "#00" + randHexDigit() + randHexDigit() + '00';
+}
+
+/////////////////////////////////////////////////////   
+function randShadeOfBlues() {
+    return "#0000" + randHexDigit() + randHexDigit();
+}
+
+/////////////////////////////////////////////////////   
+function getFilenameFromURL(url) {
+    return url.substring(url.lastIndexOf('/') + 1);
+}
+
+////////////////////////////Stack//////////////////////////
+function Stack() {
+    this._size = 0;
+    this._storage = {};
+}
+Stack.prototype.push = function (data) {
+    var size = ++this._size;
+    this._storage[size] = data;
+};
+Stack.prototype.pop = function () {
+    var size = this._size,
+        deletedData;
+    if (size) {
+        deletedData = this._storage[size];
+        delete this._storage[size];
+        this._size--;
+        return deletedData;
+    }
+}; 
---
+++ various/chess-v0-19/model.js
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////// model ///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+'use strict';
+
+function initModel() {
+
+    modelArr = new Array(FIELD_WIDTH);
+    for (let i = 0; i < modelArr.length; i++) {
+        modelArr[i] = new Array(FIELD_HEIGHT);
+        modelArr[i].fill(0)
+    }
+
+    // console.log(modelArr);
+
+    for (let i = 0; i < NUMBER_OF_MINES; i++) {
+        do {
+            var widthRand = randomInteger(0, FIELD_WIDTH - 1);
+            var heightRand = randomInteger(0, FIELD_HEIGHT - 1);
+        } while (modelArr[widthRand][heightRand] !== 0)
+        modelArr[widthRand][heightRand] = 1;
+    }
+}
+
+
+function findNumberOfNeighbourhoodMines(modelArrWidth, modelArrHeight) {
+    let total = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+            try {
+                var currValue = modelArr[modelArrWidth + i][modelArrHeight + j]
+            } catch (error) {
+                currValue = 0;
+            }
+
+            total += (!isNaN(currValue) ? currValue : 0);
+        }
+    }
+    return total;
+}
+
+function getModelArrWithNumberOfNeighbours() {
+    // let resultArr = []
+
+    //NB: similar name as global var!!!
+    let modelArrCopy = new Array(FIELD_WIDTH);
+    for (let i = 0; i < modelArrCopy.length; i++) {
+        modelArrCopy[i] = new Array(FIELD_HEIGHT);
+        modelArrCopy[i].fill(0)
+    }
+
+    for (let index1 = 0; index1 < FIELD_HEIGHT; index1++) {
+        for (let index = 0; index < FIELD_WIDTH; index++) {
+            modelArrCopy[index][index1] = findNumberOfNeighbourhoodMines(index, index1)
+        }
+    }
+    console.log(modelArrCopy)
+    return modelArrCopy
+}
+
+function isThereAMineAt(modelArrWidth, modelArrHeight) {
+    return modelArr[modelArrWidth][modelArrHeight] !== 0;
+}
