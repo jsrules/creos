a388f422401c768d34c837cbbe422f53566f3d19
jsrules <43886679+jsrules@users.noreply.github.com>
2023-12-24 15:37:39 +0200

    Add files via upload

---
+++ tmp/chess-v-0-91/controller.js
@@ -0,0 +1,27 @@
+function fieldClickHandler(td) {
+    log(td.id)
+
+
+    if (lastClickTd == null) {
+        lastClickTd = td
+        td.style.backgroundColor = COLOR_OF_LAST_MOVE_FROM
+    } else {
+        td.style.backgroundColor = COLOR_OF_LAST_MOVE_TO
+        log(currentModel.getBoardArrElementForThisId(lastClickTd.id))
+
+        if (currentModel.getBoardArrElementForThisId(lastClickTd.id).color != currentModel.getBoardArrElementForThisId(td.id).color  ) {
+            currentModel.putPiece(td.id, currentModel.getBoardArrElementForThisId(lastClickTd.id))
+            currentModel.makeFieldEmpty(lastClickTd.id)
+        }
+
+
+        $('chessBoardId').innerHTML = getHTMLStringOfTheBoard()
+        lastClickTd = null
+    }
+
+    // if (lastClickTd == td){
+    //     lastClickTd = null
+    //     $('chessBoardId').innerHTML = getHTMLStringOfTheBoard()
+    // }
+
+}
