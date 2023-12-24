7135ca43ef23c1d4af05a043d70d775c3ead7838
jsrules <43886679+jsrules@users.noreply.github.com>
2023-12-24 16:58:33 +0200

    Add files via upload

---
+++ tmp/chess-v-0-94/view.js
@@ -0,0 +1,77 @@
+function getHTMLStringOfTheBoard() {
+    // savedInitialColorsArr = []
+    let htmlToAdd = '    <table class="chessboard-table">';
+    let imgToDraw = null;
+    let gameOverMsg = null;
+    let bgColor = null;
+
+    for (let index1 = 0; index1 < FIELD_HEIGHT + 1; index1++) {
+        let numberOfRow = (FIELD_HEIGHT - index1)
+        htmlToAdd += '  <tr><th> ' + (numberOfRow ? numberOfRow : '') + ' </th>'
+        for (let index = 0; index < FIELD_WIDTH; index++) {
+
+            let startTag = ''
+            let endTag = ''
+            let fieldContent = '';
+            let letter = '' + LETTERS_ON_THE_REAL_BOARD.charAt(index);
+
+            let id = letter + numberOfRow
+            let idStr = 'id="' + id + '"'
+
+
+            if (index1 < FIELD_HEIGHT) {
+                bgColor = getInvertedFieldColor(bgColor);
+                startTag = '<td onclick="fieldClickHandler(this)"  ' + idStr + ' style=" background-color: ' + bgColor + '" ' + '>'   // +id
+                endTag = '</td>'
+                fieldContent = getImgHtml(index1, index)      //'<img src="img/wK.png">';
+                // savedInitialColorsArr[id] = bgColor
+                // log(savedInitialColorsArr[id] )
+
+            } else {
+                startTag = '<th>'
+                endTag = '</th>'
+                fieldContent = '' + letter //HORIZONTAL_FIELD_LETTERS.charAt(index);
+                //  log(7)
+            }
+            // fieldContent = index > FIELD_HEIGHT ? : ''
+
+            // let idToUse = createId(index, index1)
+            htmlToAdd += startTag + fieldContent + endTag
+            // '<img id="' + idToUse + '" style="display:inline; Font-Size:0px; Line-Height: 0px; width:30px;height:30px;margin: 0px;  padding: 0px;border: none;" border="0" src="' + imgToDraw +
+            //     '" alt="" onClick="imgClickHandler(this)" oncontextmenu="imgRightClickHandler(this)"  role="button"  tabIndex="0" />';
+
+        }
+
+
+        htmlToAdd += '</tr>'
+        bgColor = getInvertedFieldColor(bgColor);
+    }
+    htmlToAdd += ' </table>'
+    // $('fieldDivId').innerHTML = htmlToAdd
+    // gameOverMsg && myAlert(gameOverMsg)
+
+    // log(htmlToAdd)
+    return htmlToAdd
+}
+
+function getInvertedFieldColor(bgColor) {
+    return bgColor != FIRST_LEFT_UPPER_FIELD_COLOR ? FIRST_LEFT_UPPER_FIELD_COLOR : OPPOSING_LEFT_UPPER_FIELD_COLOR;
+}
+
+function getImgHtml(number, letter) {
+    if (currentModel.boardArr[number][letter] == EMPTY) {
+        return EMPTY_STR
+    }
+    let piece = currentModel.boardArr[number][letter].type
+    let color = currentModel.boardArr[number][letter].color ? 'w' : 'b'
+
+    log(currentModel.boardArr[number][letter].color)
+
+    return '<img src="img/' + color + piece + '.png">';
+
+}
+
+function updateViewFromModel() {
+    $('chessBoardId').innerHTML = getHTMLStringOfTheBoard();
+    $('UndoLastMoveId').style.display = savedModels.length ? 'block' : 'none'
+}
